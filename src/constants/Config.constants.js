ea8ce5ac9b1110cfeb0ee6017c6111b1202e079f
chebas5683243 <a20161677@pucp.edu.pe>
2022-04-20 11:23:36 -0500

    cambio deploy

--- src/constants/Config.constants.js
+++ src/constants/Config.constants.js
@@ -1,4 +1,4 @@
 export default class Config {
-  static API_URL = 'http://3a28-190-237-231-251.ngrok.io/';
+  static API_URL = 'https://3a28-190-237-231-251.ngrok.io/';
   static API_PATH = '/api/';
 }
